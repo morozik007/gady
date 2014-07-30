d53d5740a9c525611e3add07f16b18964ed02a09
Andrey Morozov <am@nxc.no>
2014-07-30 14:41:32 +0300

    photo page

---
+++ js/jcarousel.responsive.js
@@ -0,0 +1,60 @@
+(function($) {
+    $(function() {
+        var jcarousel = $('.jcarousel');
+
+        jcarousel
+            .on('jcarousel:reload jcarousel:create', function () {
+                var width = jcarousel.innerWidth();
+				
+				
+                if (width > 800) {
+		            width = width / 5;
+		        }
+				if (width > 600) {
+		            width = width / 4;
+		        }
+		        if (width > 400) {
+		            width = width / 3;
+		        }
+		        if (width > 300) {
+		            width = width / 2;
+		        }
+		        if (width > 250) {
+		            width = width / 1;
+		        }
+		        
+
+                jcarousel.jcarousel('items').css('width', width + 'px');
+            })
+            .jcarousel({
+                wrap: 'circular'
+            });
+
+        $('.jcarousel-control-prev')
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+        $('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .on('click', function(e) {
+                e.preventDefault();
+            })
+            .jcarouselPagination({
+                perPage: 1,
+                item: function(page) {
+                    return '<a href="#' + page + '">' + page + '</a>';
+                }
+            });
+    });
+})(jQuery);
--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
 $(function(){
 		
+		$.fn.startCarousel = function() {
+	  		var bodywidth = $('body').width(),
+				itemwidth = $('.mycarousel li').outerWidth(true),		
+				mycontwidth = bodywidth > itemwidth ? bodywidth - bodywidth%itemwidth : itemwidth,
+				licount = $('.mycarousel li').size(),
+				jscroll = 1;
+				
+			if(licount > mycontwidth/itemwidth){
+				jscroll =  mycontwidth/itemwidth;
+			} else {
+				jscroll = 0;
+				mycontwidth = licount * itemwidth;
+			}
+			
+			$('.mycont').width(mycontwidth);
+				
+	    	$('.mycarousel').jcarousel({
+				scroll:jscroll
+			});
+	  	};
+		
+		$(this).startCarousel();
+		
+		$(window).resize(function(){
+			$(this).startCarousel();
+		}); 
+		
+		$('.mycarousel').jcarousel({
+			scroll:1
+		});
+		
 });
